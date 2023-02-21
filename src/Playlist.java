08b7463f73a525bafdc11265f7c63d9eee8c9e50
andrewgspeed03 <112363087+andrewgspeed03@users.noreply.github.com>
2023-02-21 00:55:50 -0600

    Add files via upload
    Implemented Overloaded constructor and toString method for the Song class. Updated the default constructor addSong(int index, Song song), addSongs(Playlist playlist) and removeSong(int index) methods in the Playlist class. Also, added Playlist(String filename), addSongs(String filename), toString(), saveSongs(String filename) and getTotalTime() methods.

--- src/Playlist.java
+++ src/Playlist.java
@@ -1,11 +1,27 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.*;
 
-public class Playlist {
+/**
+ * 
+ * 
+ * @author G0161
+ * @version 5.0.2
+ * 
+ */
+
+public class Playlist{
 	
 	private ArrayList<Song> songs;
 	
 	public Playlist() {
 		// TODO: Initialize the songs field.
+		songs = new ArrayList<Song>();
+	}
+	
+	public Playlist(String filename) throws IOException {
+		this();
+		addSongs(filename);
 	}
 	
 	public int getNumSongs() {
@@ -16,7 +32,8 @@ public class Playlist {
 		if (index < 0 || index >= getNumSongs()) {
 			return null;
 		}
-		return songs.get(index);
+		else
+			return songs.get(index);
 	}
 	
 	public Song[] getSongs() {
@@ -29,10 +46,49 @@ public class Playlist {
 	
 	public boolean addSong(int index, Song song) {
 		// TODO: Update the Lab 3 method.
+		if(index >= 0 && song != null && index <= getNumSongs()) {
+			songs.add(index, song);
+			return true;
+		}
+		else
+			return false;
 	}
 	
 	public int addSongs(Playlist playlist) {
 		// TODO: Update the Lab 3 method.
+		if (playlist == null || playlist.getNumSongs() == 0) {
+			return 0;
+		}
+		else {
+			int count = playlist.getNumSongs();
+			for (int i = 0; i < count; ++i) {
+				this.addSong(playlist.getSong(i));
+			}
+			return count;
+		}
+	}
+	
+	public int addSongs(String filename) throws IOException {
+		// TODO: Update the Lab 3 method.
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		int count = 0;
+		String songname = "";
+		while (br.readLine() != null)
+			count++;
+		br.close();
+		int temp = count;
+		BufferedReader br1 = new BufferedReader(new FileReader(filename));
+		while (temp > 0) {
+			songname = br1.readLine();
+			Song s = new Song(songname);
+			addSong(s);
+			temp--;
+		}
+		br1.close();
+		if (count > 0)
+			return count;
+		else
+			return 0;			
 	}
 	
 	public Song removeSong() {
@@ -41,5 +97,61 @@ public class Playlist {
 	
 	public Song removeSong(int index) {
 		// TODO: Update the Lab 3 method.
+		if (index >= 0 && index < getNumSongs()) {
+			return songs.remove(index);
+		}
+		return null;
+	}
+	
+	public void saveSongs(String filename) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
+		bw.write(toString());
+		bw.close();
+	}
+	
+	public String toString() {
+		String rtn = "";
+		for(Song x: songs)
+			if(songs.indexOf(x) != getNumSongs() - 1)
+				rtn+= x.toString() + System.lineSeparator();
+			else
+				rtn+= x.toString();
+		return rtn;
+	}
+	
+	public int[] getTotalTime() {
+		int hrs = 0;
+		int mins = 0;
+		int secs = 0;
+		for( Song s : songs) {
+			int[] temp = Arrays.copyOf(s.getTime(), s.getTime().length);
+			if (temp.length == 3) {
+				hrs += temp[2];
+				mins += temp[1];
+				secs += temp[0];
+			}
+			else if (temp.length == 2) {
+				mins += temp[1];
+				secs += temp[0];
+			}
+			else
+				secs += temp[0];	
+		}
+		mins += secs / 60;
+		secs = secs % 60;
+		hrs += mins / 60;
+		mins = mins % 60;
+		if(hrs != 0) {
+			int[] s = {secs,mins,hrs};
+			return s;
+			}
+		else if(mins != 0) {
+			int[] s = {secs,mins};
+			return s;
+			}
+		else {
+			int[] s = {secs};
+			return s;
+			}
 	}
 }
--- src/Song.java
+++ src/Song.java
@@ -5,11 +5,28 @@ public class Song {
 	private String title;
 	private String artist;
 	private int[] time;
+	private static final String INFO_DELIMITER = "; ";
+	private static final String TIME_DELIMITER = ":";
+	private static final int IDX_TITLE = 0;
+	private static final int IDX_ARTIST = 1;
+	private static final int IDX_TIME = 2;
 	
 	public Song(String title, String artist, int[] time) {
 		this.title = title;
 		this.artist = artist;
 		this.time = Arrays.copyOf(time, time.length);
+	}
+	
+	public Song(String info) {
+		String[] splitInfo = info.split(Song.INFO_DELIMITER);
+		this.title = splitInfo[0];
+		this.artist = splitInfo[1];
+		
+		String[] splitTime = splitInfo[2].split(TIME_DELIMITER);
+		int [] temp = new int[splitTime.length];
+		for (int i = 0; i < temp.length; i++) 
+	        temp[i] = Integer.parseInt(splitTime[splitTime.length - 1 - i]);
+		this.time = Arrays.copyOf(temp, temp.length);
 	}
 	
 	public String getTitle() {
@@ -22,5 +39,20 @@ public class Song {
 	
 	public int[] getTime() {
 		return Arrays.copyOf(time, time.length);
+	}
+	
+	public String toString() {
+		String ret = getTitle() + INFO_DELIMITER +getArtist() +INFO_DELIMITER;
+		int [] time = getTime();
+		int len = time.length;
+			for(int x = len-1; x >= 0; x-- ) {
+				if (x == len-1)
+					ret+= time[x];
+				else
+					ret += String.format(":%02d", time[x]);
+			}
+		
+		return ret;
+			
 	}
 }
